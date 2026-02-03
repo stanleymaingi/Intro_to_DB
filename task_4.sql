df258ba1cf9d57d66ac7b1007ae18295d876ac8d
stanleymaingi <138701164+stanleymaingi@users.noreply.github.com>
2026-02-03 14:10:32 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,12 @@
-USE alx_book_store;
-
+-- Print full description of the Books table
 SELECT 
-    COLUMN_NAME AS 'Column',
-    COLUMN_TYPE AS 'Type',
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
     IS_NULLABLE AS 'Nullable',
     COLUMN_KEY AS 'Key',
     COLUMN_DEFAULT AS 'Default',
     EXTRA AS 'Extra'
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+  AND TABLE_NAME = 'Books'
+ORDER BY ORDINAL_POSITION;
